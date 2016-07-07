99ea81906aa0bf6b299d0379406dbb924822f76c
Igor Correa <igorbuenocorrea@gmail.com>
2016-07-07 00:44:17 -0300

    Restructure in file tree and standard deviation removed

--- BN.js
+++ BN.js
@@ -10,23 +10,72 @@ function BN() {
   this.X = new BnNode;
 
   this.A.vars = ["a"];
-  this.A.p = {"+a": 0.01, "-a": 0.99};
+  this.A.p = {
+                "+a": 0.01,
+                "-a": 0.99
+            };
+
   this.S.vars = ["s"];
-  this.S.p = {"+s": 0.5, "-s": 0.5};
+  this.S.p = {
+                "+s": 0.5,
+                "-s": 0.5
+            };
+
   this.T.vars = ["a", "t"];
-  this.T.p = {"+a,+t": 0.05, "+a,-t": 0.95, "-a,+t": 0.01, "-a,-t": 0.99};
+  this.T.p = {
+                "+a,+t": 0.05,
+                "+a,-t": 0.95,
+                "-a,+t": 0.01,
+                "-a,-t": 0.99
+              };
+
   this.L.vars = ["s", "l"];
-  this.L.p = {"+s,+l": 0.1, "+s,-l": 0.9, "-s,+l": 0.01, "-s,-l": 0.99};
+  this.L.p = {
+                "+s,+l": 0.1,
+                "+s,-l": 0.9,
+                "-s,+l": 0.01,
+                "-s,-l": 0.99
+              };
+
   this.B.vars = ["s", "b"];
-  this.B.p = {"+s,+b": 0.6, "+s,-b": 0.4, "-s,+b": 0.3, "-s,-b": 0.7};
+  this.B.p = {
+                "+s,+b": 0.6,
+                "+s,-b": 0.4,
+                "-s,+b": 0.3,
+                "-s,-b": 0.7
+              };
+
   this.E.vars = ["l", "t", "e"];
-  this.E.p = {"+l,+t,+e": 1, "+l,+t,-e": 0, "+l,-t,+e": 1, "+l,-t,-e": 0,
-              "-l,+t,+e": 1, "-l,+t,-e": 0, "-l,-t,+e": 0, "-l,-t,-e": 1};
+  this.E.p = {
+                "+l,+t,+e": 1,
+                "+l,+t,-e": 0,
+                "+l,-t,+e": 1,
+                "+l,-t,-e": 0,
+                "-l,+t,+e": 1,
+                "-l,+t,-e": 0,
+                "-l,-t,+e": 0,
+                "-l,-t,-e": 1
+              };
+
   this.D.vars = ["e", "b", "d"];
-  this.D.p = {"+e,+b,+d": 0.9, "+e,+b,-d": 0.1, "+e,-b,+d": 0.7, "+e,-b,-d": 0.3,
-              "-e,+b,+d": 0.8, "-e,+b,-d": 0.2, "-e,-b,+d": 0.1, "-e,-b,-d": 0.9};
+  this.D.p = {
+                "+e,+b,+d": 0.9,
+                "+e,+b,-d": 0.1,
+                "+e,-b,+d": 0.7,
+                "+e,-b,-d": 0.3,
+                "-e,+b,+d": 0.8,
+                "-e,+b,-d": 0.2,
+                "-e,-b,+d": 0.1,
+                "-e,-b,-d": 0.9
+              };
+
   this.X.vars = ["e", "x"];
-  this.X.p = {"+e,+x": 0.98 ,"+e,-x": 0.02 ,"-e,+x": 0.05 ,"-e,-x": 0.95};
+  this.X.p = {
+                "+e,+x": 0.98,
+                "+e,-x": 0.02,
+                "-e,+x": 0.05,
+                "-e,-x": 0.95}
+              ;
 
   this.precisao = 4;
 }
@@ -78,29 +127,33 @@ BN.prototype.query3sample = function(quantidade){
 
 BN.prototype.query3exata = function(){
   var _f1 = new BnNode(); // join em A
-  var f1 = new BnNode(); // soma em A
+  var  f1 = new BnNode(); // soma em A
   var _f2 = new BnNode(); // join em T
-  var f2 = new BnNode(); // soma em T
+  var  f2 = new BnNode(); // soma em T
   var _f3 = new BnNode(); // join em L
-  var f3 = new BnNode(); // soma em L
+  var  f3 = new BnNode(); // soma em L
   var _f4 = new BnNode(); // join em B
-  var f4 = new BnNode(); // soma em B
+  var  f4 = new BnNode(); // soma em B
   var _f5 = new BnNode(); // join em E
-  var f5 = new BnNode(); // soma em E
+  var  f5 = new BnNode(); // soma em E
 
   _f1.p = {"+t,+a": 0,"+t,-a": 0,"-t,+a": 0,"-t,+a": 0, "-t,-a": 0};
-   f1.p = {"+t": 0,"-t": 0};
+   f1.p = {   "+t": 0,   "-t": 0};
+
   _f2.p = {"+e,+l,+t": 0, "+e,+l,-t": 0, "+e,-l,+t": 0, "+e,-l,-t": 0,
            "-e,+l,+t": 0, "-e,+l,-t": 0, "-e,-l,+t": 0, "-e,-l,-t": 0, };
-   f2.p = {"+e,+l": 0, "+e,-l": 0, "-e,+l": 0, "-e,-l": 0};
+   f2.p = {"+e,+l":    0, "+e,-l":    0, "-e,+l":    0, "-e,-l":    0};
+
   _f3.p = {"+s,+e,+l": 0, "+s,+e,-l": 0, "+s,-e,+l": 0, "+s,-e,-l": 0,
            "-s,+e,+l": 0, "-s,+e,-l": 0, "-s,-e,+l": 0, "-s,-e,-l": 0};
-   f3.p = {"+s,+e": 0, "+s,-e": 0, "-s,+e": 0, "-s,-e": 0};
+   f3.p = {"+s,+e":    0, "+s,-e":    0, "-s,+e":    0, "-s,-e":    0};
+
   _f4.p = {"+d,+s,+e,+b": 0, "+d,+s,+e,-b": 0, "+d,+s,-e,+b": 0, "+d,+s,-e,-b": 0,
            "+d,-s,+e,+b": 0, "+d,-s,+e,-b": 0, "+d,-s,-e,+b": 0, "+d,-s,-e,-b": 0};
-   f4.p = {"+d,+s,+e": 0, "+d,+s,-e": 0, "+d,-s,+e": 0, "+d,-s,-e": 0};
+   f4.p = {"+d,+s,+e":    0, "+d,+s,-e":    0, "+d,-s,+e":    0, "+d,-s,-e":    0};
+
   _f5.p = {"+s,+x,+d,+e":0, "+s,+x,+d,-e":0, "-s,+x,+d,+e":0, "-s,+x,+d,-e":0};
-   f5.p = {"+s,+x,+d": 0, "-s,+x,+d": 0};
+   f5.p = {"+s,+x,+d":   0, "-s,+x,+d":   0};
   //XDadoMaisAMaisS.p = {"+a,+s,+x": 0, "+a,+s,-x": 0};
 
   //Query: P(S|+x, +d) que eh proporcional a P(S, +x, +d)
@@ -111,27 +164,27 @@ BN.prototype.query3exata = function(){
   // join e soma em A
   _f1.multiplicaTabelas([this.A.p, this.T.p], [[1],[1,0]]);
   _f1.somaEmUltimaVariavel("a", f1);
-  f1.normaliza();
+   f1.normaliza();
 
   // join e soma em T
   _f2.multiplicaTabelas([this.E.p, f1.p], [[1,2,0],[2]]);
   _f2.somaEmUltimaVariavel("t", f2);
-  f2.normaliza();
+   f2.normaliza();
 
   // join e soma em E
   _f3.multiplicaTabelas([f2.p, this.L.p], [[1,2],[0,2]]);
   _f3.somaEmUltimaVariavel("l", f3);
-  f3.normaliza();
+   f3.normaliza();
 
   // join e soma em L
   _f4.multiplicaTabelas([this.B.p, this.D.p], [[1,3],[2,3,0]]);
   _f4.somaEmUltimaVariavel("b", f4);
-  f4.normaliza();
+   f4.normaliza();
 
   // join e soma em E
   _f5.multiplicaTabelas([f4.p, this.X.p, f3.p], [[2,0,3],[3,1],[0,3]]);
   _f5.somaEmUltimaVariavel("e", f5);
-  f5.normaliza();
+   f5.normaliza();
 
   // agora faltaria multiplicar por P(+x) e P(+d) e normalizar. O resultado é
   // proporcional, portanto basta normalizar e mostrar o resultado.
@@ -183,20 +236,22 @@ BN.prototype.query2sample = function(quantidade){
 
 BN.prototype.query2exata = function(){
   var _f1 = new BnNode(); // join em T
-  var f1 = new BnNode(); // soma em T
+  var  f1 = new BnNode(); // soma em T
   var _f2 = new BnNode(); // join em L
-  var f2 = new BnNode(); // soma em L
+  var  f2 = new BnNode(); // soma em L
   var _f3 = new BnNode(); // join em E
-  var f3 = new BnNode(); // soma em E
+  var  f3 = new BnNode(); // soma em E
 //  var XDadoMaisAMaisS = new BnNode();
 
-  _f1.p = {"+a,+e,+l,+t": 0,"+a,+e,+l,-t": 0,"+a,+e,-l,+t": 0,"+a,+e,-l,-t": 0,
-           "+a,-e,+l,+t": 0,"+a,-e,+l,-t": 0,"+a,-e,-l,+t": 0,"+a,-e,-l,-t": 0};
-   f1.p = {"+a,+e,+l": 0,"+a,+e,-l": 0,"+a,-e,+l": 0,"+a,-e,-l": 0};
+  _f1.p = {"+a,+e,+l,+t": 0, "+a,+e,+l,-t": 0, "+a,+e,-l,+t": 0, "+a,+e,-l,-t": 0,
+           "+a,-e,+l,+t": 0, "+a,-e,+l,-t": 0, "+a,-e,-l,+t": 0, "+a,-e,-l,-t": 0};
+   f1.p = {"+a,+e,+l":    0, "+a,+e,-l":    0, "+a,-e,+l":    0, "+a,-e,-l":    0};
+
   _f2.p = {"+s,+a,+e,+l": 0, "+s,+a,+e,-l": 0, "+s,+a,-e,+l": 0, "+s,+a,-e,-l": 0};
-   f2.p = {"+s,+a,+e": 0, "+s,+a,+e": 0, "+s,+a,-e": 0, "+s,+a,-e": 0};
-  _f3.p = {"+x,+a,+s,+e": 0,"+x,+a,+s,-e": 0,"-x,+a,+s,+e": 0,"-x,+a,+s,-e": 0};
-   f3.p = {"+x,+a,+s": 0,"+x,+a,+s": 0,"-x,+a,+s": 0,"-x,+a,+s": 0};
+   f2.p = {"+s,+a,+e":    0, "+s,+a,+e":    0, "+s,+a,-e":    0, "+s,+a,-e":    0};
+
+  _f3.p = {"+x,+a,+s,+e": 0, "+x,+a,+s,-e": 0, "-x,+a,+s,+e": 0, "-x,+a,+s,-e": 0};
+   f3.p = {"+x,+a,+s":    0, "+x,+a,+s":    0, "-x,+a,+s":    0, "-x,+a,+s": 0};
 //  XDadoMaisAMaisS.p = {"+a,+s,+x": 0, "+a,+s,-x": 0};
 
   //Query: P(X|+a, +s) que eh proporcional a P(X, +a, +s)
@@ -261,9 +316,9 @@ BN.prototype.query1sample = function(quantidade){
 
 BN.prototype.query1exata = function(){
   var probTA = new BnNode();
-  var probT = new BnNode();
+  var probT  = new BnNode();
   probTA.p = {"+t,+a": 0, "+t,-a": 0, "-t,+a": 0, "-t,-a": 0};
-  probT.p = {"+t": 0, "-t": 0};
+  probT.p  = {"+t":    0, "-t":    0};
 
   probTA.multiplicaTabelas([this.T.p, this.A.p], [[1,0],[1]]);
   probTA.somaEmUltimaVariavel("a", probT);
@@ -300,8 +355,7 @@ BN.prototype.montaTabela = function(resultadoExato, resultadoAproximado,
   }
   tabelaHtml += "</table>"
 
-  tabelaHtml += "<p>Erro médio (%): " + difMedia + "</p>"
-  tabelaHtml += "<p>Desvio padrão (%): " + desvioPadrao + "</p>"
+  tabelaHtml += "<p>Erro médio em 100 execuções do método aproximado (%): " + difMedia + "</p>"
 
   return tabelaHtml;
 }
